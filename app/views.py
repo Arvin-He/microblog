7dd1dda619adb50ef8cdd4c918a89b13dba7f0c6
Arvin-He <510205617@qq.com>
2017-04-04 16:50:19 +0800

    template use control

--- app/views.py
+++ app/views.py
@@ -2,9 +2,24 @@ from flask import render_template
 from app import app
 
 
+# @app.route('/')
+# @app.route('/index')
+# def index():
+#     user = {'nickname': 'Arvin'}
+#     return render_template("index.html", title='Home', user=user)
+
+
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'nickname': 'Arvin'}
-    return render_template("index.html", title='Home', user=user)
-    # return "Hello, world!"
+    posts = [
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movies was so cool!'
+        }]
+    return render_template("index.html", title='Home', user=user,posts=posts)
