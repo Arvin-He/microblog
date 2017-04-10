686397afc7dbf6e8c9ce600ac3a1ce8c5941e31e
Arvin-He <510205617@qq.com>
2017-04-10 22:27:09 +0800

    add wtf

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
 
 # @app.route('/')
@@ -23,3 +24,8 @@ def index():
             'body': 'The Avengers movies was so cool!'
         }]
     return render_template("index.html", title='Home', user=user,posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form = form)
---
+++ config.py
@@ -0,0 +1,4 @@
+# 为了激活跨站点请求伪造保护
+CSRF_ENABLED = True
+# 仅当CSRF激活时才需要,用来建立一个加密令牌,用于验证一个表单
+SECRET_KEY = 'you-will-never-guess'
