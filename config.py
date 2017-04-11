740f13fa883fac832fb2b004541b4ed3d77f5610
Arvin-He <510205617@qq.com>
2017-04-11 22:52:49 +0800

    update web form

--- config.py
+++ config.py
@@ -2,3 +2,10 @@
 CSRF_ENABLED = True
 # 仅当CSRF激活时才需要,用来建立一个加密令牌,用于验证一个表单
 SECRET_KEY = 'you-will-never-guess'
+
+OPENID_PROVIDERS = [
+    { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
+    { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
+    { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
+    { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
+    { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
